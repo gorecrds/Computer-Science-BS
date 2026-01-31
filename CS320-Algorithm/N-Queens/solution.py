b64c368f99d9e23f604bb94a72ac909bba47c4ef
Igor Cardoso <gore.crds@gmail.com>
2026-01-31 10:09:45 -0800

    pep8 index needs to start with i commit

--- CS320-Algorithm/N-Queens/solution.py
+++ CS320-Algorithm/N-Queens/solution.py
@@ -1,51 +1,59 @@
 def nQueensAll(n: int) -> list[list[tuple[int, int]]]:  # pylint: disable=invalid-name
     if n < 4:
         raise ValueError("Value Error: n must be equal to or greater than 4.")
+
     cols = set()
     pos_diag_id = set()  # i_row + i_col
     neg_diag_id = set()  # i_row - i_col
     result = []
     queen_position = [None] * n
 
-
-    def remove_queen(row: int) -> int:
+    def remove_queen(i_row: int) -> int:
         """Remove queen from given row. Returns the removed column."""
-        col = queen_position[row]
-        cols.remove(col)
-        pos_diag_id.remove(row + col)
-        neg_diag_id.remove(row - col)
-        queen_position[row] = None
-        return col
+        i_col = queen_position[i_row]
+        cols.remove(i_col)
+        pos_diag_id.remove(i_row + i_col)
+        neg_diag_id.remove(i_row - i_col)
+        queen_position[i_row] = None
+        return i_col
 
     def backtrack() -> None:
-        index_row=0
-        index_col=0
-        while index_row < n:
-            if index_row ==0 and index_col >= n:
+        i_row = 0
+        i_col = 0
+
+        while i_row < n:
+            if i_row == 0 and i_col >= n:
                 return
-            if index_col >= n:
-                index_row-=1
-                index_col = remove_queen(index_row) + 1
+
+            if i_col >= n:
+                i_row -= 1
+                i_col = remove_queen(i_row) + 1
                 continue
 
-            index_pos_diag = index_row + index_col
-            index_neg_diag = index_row - index_col
+            i_pos_diag = i_row + i_col
+            i_neg_diag = i_row - i_col
 
-            if (index_col in cols or index_pos_diag in pos_diag_id or index_neg_diag in neg_diag_id):
-                index_col += 1
+            if (
+                i_col in cols
+                or i_pos_diag in pos_diag_id
+                or i_neg_diag in neg_diag_id
+            ):
+                i_col += 1
                 continue
 
-            queen_position[index_row] = index_col
-            cols.add(index_col)
-            pos_diag_id.add(index_pos_diag)
-            neg_diag_id.add(index_neg_diag)
-            index_col = 0
-            index_row += 1
+            queen_position[i_row] = i_col
+            cols.add(i_col)
+            pos_diag_id.add(i_pos_diag)
+            neg_diag_id.add(i_neg_diag)
 
-            if index_row >= n:
-                result.append([(r, c) for r, c in enumerate(queen_position)])
-                index_row -= 1
-                index_col = remove_queen(index_row) + 1
+            i_col = 0
+            i_row += 1
+
+            if i_row >= n:
+                result.append([(i_r, i_c) for i_r, i_c in enumerate(queen_position)])
+                i_row -= 1
+                i_col = remove_queen(i_row) + 1
+
     backtrack()
     return result
 
