bbe3ae22116e173d39fd0e38bb6d3b0abc058e63
Igor Cardoso <gore.crds@gmail.com>
2026-02-14 17:39:27 -0800

    initial setup

---
+++ CS320-Algorithm/Lists_as_trees/solutions.py
@@ -0,0 +1,18 @@
+from treelist import inOrderRecurse
+
+def findKey(k, t):
+    pass
+def addKey(k, t):
+    pass
+
+def deleteKey(k, t):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    tree=[]
+    addKey(5, tree)
+    addKey(3, tree)
+    addKey(7, tree)
